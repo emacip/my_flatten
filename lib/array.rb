0892b319d7e845173177b7b0f913e16c4eccc2a3
emacip <emacipbelmonte@gmail.com>
2015-10-06 12:21:08 +0100

    Refactor my_flatten to use inject function and concat the arrays

--- lib/array.rb
+++ lib/array.rb
@@ -1,10 +1,8 @@
 class Array
   def my_flatten
-    new_ary = []
-    each do |val|
-      new_ary << val if not val.kind_of?(Array)
-      new_ary.concat(val.my_flatten) if val.kind_of?(Array)
+    inject([]) do |new_ary, e|
+      new_ary += e.is_a?(Array) ? e.my_flatten : [e]
     end
-    return new_ary
-  end	
+  end
 end
+
