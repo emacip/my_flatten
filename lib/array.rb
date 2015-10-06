549fca58ef2dc6c97c64f070e5d0493798f9c3f4
Enrique Macip <emacipbelmonte@gmail.com>
2015-10-06 10:20:20 +0100

    Update array.rb
    format correctly class and delete require byebug

--- lib/array.rb
+++ lib/array.rb
@@ -1,12 +1,10 @@
-require 'byebug'
-
 class Array
-	def my_flatten
+  def my_flatten
     new_ary = []
     each do |val|
       new_ary << val if not val.kind_of?(Array)
       new_ary.concat(val.my_flatten) if val.kind_of?(Array)
     end
     return new_ary
-	end	
+  end	
 end
